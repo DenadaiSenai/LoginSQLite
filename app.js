555e28c4b6288795c392bebe964abda71bc5a238
Marcio <125585681+DenadaiSenai@users.noreply.github.com>
2023-11-28 14:48:33 -0300

    Ajustando para a versão MySQL

--- app.js
+++ app.js
@@ -1,60 +1,162 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const mysql = require('mysql');
 const session = require('express-session');
-const authRouter = require('./routes/authController');
-const db = require('./database');
-
+const bodyParser = require('body-parser');
 const app = express();
-const port = 3000;
 
-// Configurar express-session
-app.use(session({
-  secret: 'suaChaveSecreta',
-  resave: false,
-  saveUninitialized: true
-}));
+// Configurar a conexão com o banco de dados MySQL
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'mydb',
+});
 
-// Servir arquivos estáticos (por exemplo, imagens, folhas de estilo)
-app.use(express.static('public'));
+db.connect((err) => {
+    if (err) {
+        console.error('Erro ao conectar ao banco de dados:', err);
+        throw err;
+    }
+    console.log('Conexão com o banco de dados MySQL estabelecida.');
+});
+
+// Configurar a sessão
+app.use(
+    session({
+        secret: 'Escreva aqui a senha para criptografar as sessões.',
+        resave: true,
+        saveUninitialized: true,
+    })
+);
+
+// Configuração de pastas com aquivos estáticos
+//app.use('/img', express.static(__dirname + '/img'))
+app.use('/', express.static(__dirname + '/static'))
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.set('view engine', 'ejs'); // Definir o mecanismo de visualização como EJS
-
-// Middleware para verificar a sessão do usuário
-app.use((req, res, next) => {
-  if (req.session.user) {
-    res.locals.user = req.session.user;
-  }
-  next();
-});
-
-// rota para home
-app.get('/', (req, res) => {
-  res.render('index');
-});
+// Configurar EJS como o motor de visualização
+app.set('view engine', 'ejs');
 
 // Rota para a página de login
+app.get('/', (req, res) => {
+    // Passe a variável 'req' para o template e use-a nas páginas para renderizar partes do HTML conforme determinada condição
+    // Por exemplo de o usuário estive logado, veja este exemplo no arquivo views/partials/header.ejs
+    res.render('pages/index', { req: req });
+    console.log(`${req.session.username ? `Usuário ${req.session.username} logado no IP ${req.connection.remoteAddress}` : 'Usuário não logado.'}  `);
+    //console.log(req.connection)
+    ;
+});
+
+// Página de login
 app.get('/login', (req, res) => {
-  res.render('login');
+    res.render('pages/login', { req: req });
 });
 
-// Rota para a página de registro
-app.get('/register', (req, res) => {
-  res.render('register');
+app.get('/about', (req, res) => {
+    res.render('pages/about', { req: req })
 });
 
-// Rota protegida para a dashboard
+// Rota para processar o formulário de login
+app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+
+    const query = 'SELECT * FROM users WHERE username = ? AND password = SHA1(?)';
+
+    db.query(query, [username, password], (err, results) => {
+        if (err) throw err;
+
+        if (results.length > 0) {
+            req.session.loggedin = true;
+            req.session.username = username;
+            res.redirect('/dashboard');
+        } else {
+            // res.send('Credenciais incorretas. <a href="/">Tente novamente</a>');
+            res.redirect('/login_failed');
+        }
+    });
+});
+
+// Rotas para cadastrar
+app.get('/cadastrar', (req, res) => {
+    if (!req.session.loggedin) {
+        res.render('pages/cadastrar', { req: req });
+    } else {
+        res.redirect('pages/dashboard', { req: req });
+    }
+});
+
+app.post('/cadastrar', (req, res) => {
+    const { username, password } = req.body;
+
+    // Verifica se o usuário já existe
+    const query = 'SELECT * FROM users WHERE username = ? AND password = SHA1(?)';
+    db.query(query, [username, password], (err, results) => {
+        if (err) throw err;
+        // Caso usuário já exista no banco de dados, redireciona para a página de cadastro inválido
+        if (results.length > 0) {
+            console.log(`Usuário ${username} já existe no banco de dados. redirecionando`);
+            res.redirect('/register_failed');
+        } else {
+            // Cadastra o usuário caso não exista
+            const query = 'INSERT INTO users (username, password) VALUES (?, SHA1(?))';
+            console.log(`POST /CADASTAR -> query -> ${query}`);
+            db.query(query, [username, password], (err, results) => {
+                console.log(results);
+                //console.log(`POST /CADASTAR -> results -> ${results}`);
+
+                if (err) {
+                    console.log(`ERRO NO CADASTRO: ${err}`);
+                    throw err;
+                }
+                if (results.affectedRows > 0) {
+                    req.session.loggedin = true;
+                    req.session.username = username;
+                    res.redirect('/register_ok');
+                }
+            });
+        }
+    });
+});
+
+app.get('/register_failed', (req, res) => {
+    res.render('pages/register_failed', { req: req });
+});
+
+app.get('/register_ok', (req, res) => {
+    res.render('pages/register_ok', { req: req });
+});
+
+app.get('/login_failed', (req, res) => {
+    res.render('pages/login_failed', { req: req });
+});
+
+// Rota para a página do painel
 app.get('/dashboard', (req, res) => {
-  if (res.locals.user) {
-    res.render('dashboard', { user: res.locals.user });
-  } else {
-    res.redirect('/login');
-  }
+    //
+    //modificação aqui
+    if (req.session.loggedin) {
+        //res.send(`Bem-vindo, ${req.session.username}!<br><a href="/logout">Sair</a>`);
+        // res.sendFile(__dirname + '/index.html');
+        res.render('pages/dashboard', { req: req });
+    } else {
+        res.send('Faça login para acessar esta página. <a href="/">Login</a>');
+    }
 });
 
-app.use('/auth', authRouter);
+// Rota para fazer logout
+app.get('/logout', (req, res) => {
+    req.session.destroy(() => {
+        res.redirect('/');
+    });
+});
 
-app.listen(port, () => {
-  console.log(`Servidor rodando em http://localhost:${port}`);
+// Rota de teste
+app.get('/teste', (req, res) => {
+    res.render('pages/teste', { req: req });
+});
+
+
+app.listen(3000, () => {
+    console.log('Servidor rodando na porta 3000');
 });
